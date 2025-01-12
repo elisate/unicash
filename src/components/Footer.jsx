d76fca81bd1cba3d39a1959e97701fb2f4010057
Kgadrw <kalisagad05@gmail.com>
2025-01-12 17:35:41 +0200

    collection of footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,13 +7,9 @@ const Footer = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-8">
           <div className="text-center md:text-left">
-            <img
-              src="/logo.png"
-              alt="IGA Tech Logo"
-              className="h-12 w-auto mb-4 mx-auto md:mx-0"
-            />
             <p className="text-lg">
-              Empowering communities with digital skills. Join the revolution.
+              Iga tech is Empowering communities with digital skills. Join the
+              revolution.
             </p>
           </div>
           <div className="text-center md:text-left">
